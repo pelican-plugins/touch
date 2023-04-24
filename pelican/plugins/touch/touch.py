c667f1816964884198bfe3ed0208ad833abcc374
Justin Mayer <entroP@gmail.com>
2023-04-24 15:00:33 +0200

    Add docstrings to plugin functions

--- pelican/plugins/touch/touch.py
+++ pelican/plugins/touch/touch.py
@@ -8,12 +8,14 @@ logger = logging.getLogger(__name__)
 
 
 def set_file_utime(path, datetime):
+    """Set modified time (mtime) of specified file path to the current time."""
     mtime = time.mktime(datetime.timetuple())
     logger.info("touching %s", path)
     os.utime(path, (mtime, mtime))
 
 
 def touch_file(path, context):
+    """Set modified time (mtime) of Pelican content files that contain dates."""
     content = context.get("article", context.get("page"))
     page = context.get("articles_page")
     dates = context.get("dates")
@@ -27,9 +29,11 @@ def touch_file(path, context):
 
 
 def touch_feed(path, context, feed):
+    """Set modified time (mtime) of feeds."""
     set_file_utime(path, max(x["pubdate"] for x in feed.items))
 
 
 def register():
+    """Register plugin with specified Pelican signals."""
     signals.content_written.connect(touch_file)
     signals.feed_written.connect(touch_feed)
